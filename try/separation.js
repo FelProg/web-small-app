e2a747d7090455fa8bd1140cf1a0ae29c7e9eb81
FelProg <felixgerman@hotmail.com>
2022-05-20 20:56:50 -0700

    regex will admit the , and the '

---
+++ try/separation.js
@@ -0,0 +1,15 @@
+
+
+let str = 'using System; class Program {  public static void Main (string[] args) {    Console.WriteLine ("Hello World");  }}';
+//let str = 'console.log("Hola mundo");';
+
+let result = str.split(/([\s\[\];,\(\).\{\}\"\'])/);
+
+result = result.filter(function(e){return e.trim() != '';})
+console.log(result);
+result.forEach(element => {
+    console.log(element);    
+});
+
+let using = '2';
+console.log(using);
